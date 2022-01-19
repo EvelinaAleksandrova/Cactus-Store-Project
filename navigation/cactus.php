95d9b6d3119473f005d32d8dd1b4aa436bf28abc
evelina <evety.1dtvd@gmail.com>
2022-01-19 17:07:46 +0200

    Add cactus type in store.

--- navigation/cactus.php
+++ navigation/cactus.php
@@ -1,63 +1,61 @@
 
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Cactus Store</title>
-        <link rel="icon" type="image/x-icon" href="/images/cactus%20(1).png">
 
-        <link rel="stylesheet" href="/css/style.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cactus Store</title>
+    <link rel="icon" type="image/x-icon" href="../images/cactus%20(1).png">
 
-        <link rel="home" href="/navigation/home.html">
-        <link rel="article" href="/navigation/articles.html">
-        <link rel="about" href="/navigation/about-us.html">
-        <link rel="contact" href="/navigation/contact.html">
+    <link rel="stylesheet" href="/css/style.css">
+<link rel="style" href="/css/cactus.css">
 
-        <link rel="cactus" href="/navigation/cactus.php">
+    <link rel="home" href="/navigation/home.html">
+    <link rel="article" href="/navigation/articles.html">
+    <link rel="about" href="/navigation/about-us.html">
+    <link rel="contact" href="/navigation/contact.html">
 
-    </head>
-    <body>
-    <!--- Navigation Start here ---->
-    <div class="topnav" id="myTopnav">
-        <a href="/navigation/home.html" class="active">
-            <div class="logo-image">
-                <img src="/images/cactus%20(2).png" class="img-fluid" height="35px">
-            </div>
-        </a>
-        <a href="/navigation/articles.html">Articles</a>
-        <a href="/navigation/contact.html">Contacts</a>
-        <a href="/navigation/about-us.html">About us</a>
-        <div class="dropdown">
-            <button class="dropbtn">Store
-                <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="dropdown-content">
-                <a href="/navigation/cactus.php">Cactus</a>
-                <a href="#succulents">Succulents</a>
-                <a href="#ground">Ground</a>
-                <a href="#pots">Pots</a>
-            </div>
+    <link rel="cactus" href="/navigation/cactus.php">
+
+</head>
+<body>
+<!--- Navigation Start here ---->
+<div class="topnav" id="myTopnav">
+    <a href="home.html" class="active">
+        <div class="logo-image">
+            <img src="/images/cactus%20(2).png" class="img-fluid" height="35px">
+        </div>
+    </a>
+    <a href="articles.html">Articles</a>
+    <a href="contact.html">Contacts</a>
+    <a href="about-us.html">About us</a>
+    <div class="dropdown">
+        <button class="dropbtn">Store
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+
+            <a href="cactus.php">Cactus</a>
+            <a href="#succulents">Succulents</a>
+            <a href="#ground">Ground</a>
+            <a href="#pots">Pots</a>
+
         </div>
         <a href="/login.php">Login</a>
-        <a href="#" class="cart">
-            <div class="cart-image">
-                <img src="/images/shopping-cart.png" class="img-fluid" >
-            </div>
-        </a>
-        <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
     </div>
+    <a href="#" class="cart">
+        <div class="cart-image">
+            <img src="/images/shopping-cart.png" class="img-fluid" >
+        </div>
+    </a>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+</div>
 
+<div class="footer">
+    <p>&copy; 2022 Cactus Store. All rights reserved.</p>
+</div>
+<script src="/script.js"></script>
 
-    <h1>Hello</h1>
-
-
-    <div class="footer">
-        <p>&copy; 2022 Cactus Store. All rights reserved.</p>
-    </div>
-
-    <script src="/script.js"></script>
-    </body>
-    </html>
-
-<?php
+</body>
+</html>
